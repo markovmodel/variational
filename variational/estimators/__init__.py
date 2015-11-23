a521b8614c42f2a01432964d20d87011feb576d0
noe <frank.noe@fu-berlin.de>
2015-11-23 01:33:30 +0100

    [variational.estimators] absolute imports
    Former-commit-id: 9d1ae683efa99e7e11f859ba755736cb2ffaade9

--- variational/estimators/__init__.py
+++ variational/estimators/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
-__author__ = 'noe'
 
-from moments import moments_XX, moments_XXXY, moments_block
-from moments import covar, covars
-from running_moments import RunningCovar, running_covar
+from variational.estimators.moments import moments_XX, moments_XXXY, moments_block
+from variational.estimators.moments import covar, covars
+from variational.estimators.running_moments import RunningCovar, running_covar
